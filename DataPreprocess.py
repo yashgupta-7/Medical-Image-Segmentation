5d3c6e0eeb3a1dd9656eaa79af11faf8f451d17e
yashgupta-7 <>
2020-04-14 12:22:34 +0530

    ádded validation accuracy visualization

--- DataPreprocess.py
+++ DataPreprocess.py
@@ -60,10 +60,12 @@ for img in img_list:
         io.imsave("data/training/"+str(count)+"_noise.jpg",img_as_ubyte(noisyimg1))
         
 ######################################################################################################
-#################################################TEST DATA############################################
+#################################################VALIDATION DATA######################################
 ######################################################################################################
 
 count = 0
+imgs_folder = "data/validation/"
+labels_folder = "data/validation_manual/"
 for img in img_list:
     image = Image.open("STARE/stare-images/"+img)
     label = Image.open("STARE/labels-vk/"+img[0:6]+".vk.ppm")
@@ -75,8 +77,8 @@ for img in img_list:
         x,y =  np.random.randint(0,w-patch_size),np.random.randint(0,h-patch_size)
         img1 = image.crop((x, y, x+patch_size, y+patch_size))
         lab1 = label.crop((x, y, x+patch_size, y+patch_size))
-        img1 = img1.save("data/testing/"+str(count)+"_orig.jpg")
-        lab1 = lab1.save("data/testing_manual/"+str(count)+"_orig.jpg")
+        img1 = img1.save(imgs_folder+str(count)+"_orig.jpg")
+        lab1 = lab1.save(labels_folder+str(count)+"_orig.jpg")
         
         # Rotation
         w, h = image.size
@@ -86,16 +88,16 @@ for img in img_list:
         angle = random.choice([90,180,270])
         img1 = img1.rotate(angle)
         lab1 = lab1.rotate(angle)
-        img1 = img1.save("data/testing/"+str(count)+"_rot.jpg")
-        lab1 = lab1.save("data/testing_manual/"+str(count)+"_rot.jpg")
+        img1 = img1.save(imgs_folder+str(count)+"_rot.jpg")
+        lab1 = lab1.save(labels_folder+str(count)+"_rot.jpg")
         
         # Adding Noise with sigma=0.05
         w, h = image.size
         x,y =  np.random.randint(0,w-patch_size),np.random.randint(0,h-patch_size)
         img1 = image.crop((x, y, x+patch_size, y+patch_size))
         lab1 = label.crop((x, y, x+patch_size, y+patch_size))
-        img1 = img1.save("data/testing/"+str(count)+"_noise.jpg")
-        lab1 = lab1.save("data/testing_manual/"+str(count)+"_noise.jpg")
-        img1 = io.imread("data/testing/"+str(count)+"_noise.jpg")
+        img1 = img1.save(imgs_folder+str(count)+"_noise.jpg")
+        lab1 = lab1.save(labels_folder+str(count)+"_noise.jpg")
+        img1 = io.imread(imgs_folder+str(count)+"_noise.jpg")
         noisyimg1 = random_noise(img1,var=0.05**2)
-        io.imsave("data/testing/"+str(count)+"_noise.jpg",img_as_ubyte(noisyimg1))
+        io.imsave(imgs_folder+str(count)+"_noise.jpg",img_as_ubyte(noisyimg1))
